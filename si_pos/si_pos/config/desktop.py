8a03570b2437eb2908eaeea22434c12680c243b6
ERPNext Oman <154664429+ERPNextOman@users.noreply.github.com>
2026-05-18 19:56:56 +0400

    Add desktop config

---
+++ si_pos/si_pos/config/desktop.py
@@ -0,0 +1,13 @@
+from frappe import _
+
+
+def get_data():
+    return [
+        {
+            "module_name": "SI POS",
+            "color": "blue",
+            "icon": "octicon octicon-credit-card",
+            "type": "module",
+            "label": _("SI POS"),
+        }
+    ]
